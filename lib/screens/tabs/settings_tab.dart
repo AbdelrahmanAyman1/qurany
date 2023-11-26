ce67664b80af783defef15735381ee4b105ddb14
boody <boodayman936@gmail.com>
2023-11-26 18:27:49 +0200

    make the container of theming and language open a bottomSheet

--- lib/screens/tabs/settings_tab.dart
+++ lib/screens/tabs/settings_tab.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:qurany/theme/my_theme_data.dart';
 
-class SettingsTab extends StatelessWidget {
+class SettingsTab extends StatefulWidget {
   const SettingsTab({super.key});
 
+  @override
+  State<SettingsTab> createState() => _SettingsTabState();
+}
+
+class _SettingsTabState extends State<SettingsTab> {
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -12,29 +17,71 @@ class SettingsTab extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           const Text('Language'),
-          Container(
-            width: double.infinity,
-            padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
-            margin: const EdgeInsets.symmetric(horizontal: 20),
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(12),
-                border: Border.all(color: MyThemeData.primaryColor)),
-            child: const Text('English'),
+          InkWell(
+            onTap: () => openLanguageBottomSheet(),
+            child: Container(
+              width: double.infinity,
+              padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+              margin: const EdgeInsets.symmetric(horizontal: 20),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12),
+                  border: Border.all(color: MyThemeData.primaryColor)),
+              child: const Text('English'),
+            ),
           ),
           const SizedBox(
             height: 18,
           ),
           const Text('Theming'),
-          Container(
-            width: double.infinity,
-            padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
-            margin: const EdgeInsets.symmetric(horizontal: 20),
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(12),
-                border: Border.all(color: MyThemeData.primaryColor)),
-            child: const Text('Light'),
+          InkWell(
+            onTap: () => openThemingBottomSheet(),
+            child: Container(
+              width: double.infinity,
+              padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+              margin: const EdgeInsets.symmetric(horizontal: 20),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12),
+                  border: Border.all(color: MyThemeData.primaryColor)),
+              child: const Text('Light'),
+            ),
           )
         ],
+      ),
+    );
+  }
+
+  openLanguageBottomSheet() {
+    showModalBottomSheet(
+      context: context,
+      isScrollControlled: true,
+      builder: (context) {
+        return Container(
+          height: MediaQuery.of(context).size.height * .9,
+        );
+      },
+      shape: const OutlineInputBorder(
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(18),
+          topRight: Radius.circular(18),
+        ),
+      ),
+    );
+  }
+
+  openThemingBottomSheet() {
+    showModalBottomSheet(
+      isScrollControlled: true,
+      context: context,
+      builder: (context) {
+        return Container(
+          height: MediaQuery.of(context).size.height * .9,
+        );
+      },
+      shape: const OutlineInputBorder(
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(18),
+          topRight: Radius.circular(18),
+        ),
       ),
     );
   }
