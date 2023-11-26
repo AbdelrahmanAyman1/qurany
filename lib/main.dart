2e20f5b7b2a89df43bcbe1bf2786f82dfde2aab1
boody <boodayman936@gmail.com>
2023-11-26 13:31:37 +0200

    add Localization add en language file

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:qurany/screens/Hadeth_details.dart';
 import 'package:qurany/screens/home_screen.dart';
 import 'package:qurany/screens/sura_details.dart';
@@ -14,12 +15,21 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('en'), // English
+        Locale('ar'), // Arabic
+      ],
       debugShowCheckedModeBanner: false,
       initialRoute: HomeScreen.routeName,
       routes: {
         HomeScreen.routeName: (context) => const HomeScreen(),
         SuraDetailsScreen.routeName: (context) => const SuraDetailsScreen(),
-        HadethDetailsScreen.routeName: (context) => HadethDetailsScreen()
+        HadethDetailsScreen.routeName: (context) => const HadethDetailsScreen()
       },
       theme: MyThemeData.lightTheme,
       darkTheme: MyThemeData.darkTheme,
