1fc0ab69f7952dea97ac88ab750a56242928f721
boody <boodayman936@gmail.com>
2023-11-28 00:54:54 +0200

    add sebha image to sebha_tab.dart

--- lib/screens/tabs/sebha_tab.dart
+++ lib/screens/tabs/sebha_tab.dart
@@ -5,6 +5,21 @@ class SebhaTab extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Stack(
+          alignment: Alignment.topCenter,
+          children: [
+            const SizedBox(height: 30),
+            Image.asset('assets/images/head_of_seb7a.png'),
+            Padding(
+                padding:
+                    EdgeInsets.all(MediaQuery.of(context).size.height * .088),
+                child: Image.asset('assets/images/body_of_seb7a.png'))
+          ],
+        ),
+      ],
+    );
   }
 }
